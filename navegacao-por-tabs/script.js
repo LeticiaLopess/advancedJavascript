1874124c535bc4b36d7f467d6983b366f2c19aa9
Leticia Lopes <leticialopes236@gmail.com>
2023-04-20 15:42:33 -0300

    scroll suave

--- navegacao-por-tabs/script.js
+++ navegacao-por-tabs/script.js
@@ -9,40 +9,96 @@ dos mesmos
 Ai vai ser tipo: cliquei na raposa, vai adicionar uma classe no texto da raposa,
 teremos uma relação entre um e outro
 */
-
-const tabMenu = document.querySelectorAll('.js-tabmenu li'); // retorna cada li dessa ul
-const tabContent = document.querySelectorAll('.js-tabcontent section') // tem uma section pra cada animal
-
-console.log(tabContent.length) // 0 - por isso
-console.log(tabMenu.length) // 6
-console.log(!!tabMenu.length) // true - lembre que o "!!" antes verifica se é true ou false
-console.log(!!tabMenu.length) // true
-
-if(tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add('ativo') // para sempre ter o primeiro elemento com a classe "ativo", ou posso colocar direto no HTML
-
-  tabContent[0] // primeira section
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li'); // retorna cada li dessa ul
+  const tabContent = document.querySelectorAll('.js-tabcontent section') // tem uma section pra cada animal
   
-  function activeTab(index) { // index da tab que eu quero
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    })
-    tabContent[index].classList.add('ativo');
+  console.log(tabContent.length) // 0 - por isso
+  console.log(tabMenu.length) // 6
+  console.log(!!tabMenu.length) // true - lembre que o "!!" antes verifica se é true ou false
+  console.log(!!tabMenu.length) // true
+  
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo') // para sempre ter o primeiro elemento com a classe "ativo", ou posso colocar direto no HTML
+  
+    tabContent[0] // primeira section
+    
+    function activeTab(index) { // index da tab que eu quero
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      })
+      tabContent[index].classList.add('ativo');
+    }
+    
+    // console.log(activeTab(0))
+    
+    // (item, index e array)
+    tabMenu.forEach((itemMenu, index) => { // index das duas tabelas serão respectivos, devem estar organizadas em ordem certa sempre
+      itemMenu.addEventListener('click', () => {
+        activeTab(index)
+      })
+    }) 
   }
-  
-  // console.log(activeTab(0))
-  
-  // (item, index e array)
-  tabMenu.forEach((itemMenu, index) => { // index das duas tabelas serão respectivos, devem estar organizadas em ordem certa sempre
-    itemMenu.addEventListener('click', () => {
-      activeTab(index)
-    })
-  }) 
-  
 }
+initTabNav(); // se eu não chamar, não ativa a função
 
 
 // agora mudaremos o CSS para só apenas quem estiver ativo aparecer
+
+          /*
+          .animais-lista li {
+            cursor: pointer;
+          }
+
+          .js-tabcontent section { // esconde de leitores de tela também ai teríamos que ver outra forma
+          display: none;
+          }
+
+          .js-tabcontent section.ativo {
+            display: block !important;
+            animation: show .5s forwards; // o forwards deixará o texto na sua posição final [to] do keyframe show 
+          }
+
+          @keyframes show { // animação com CSS 
+            from {
+              opacity: 0;
+              transform: matrix3d();
+              transform: translate3d(-30px, 0, 0);
+            }
+            to {
+              opacity: 1;
+              transform: translate3d(0,0,0);
+            }
+          }
+          */
+
+
+
+// ACORDION LIST
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt'); // acessando o título dessa lista de perguntas e respostas
+  const activeClass = 'ativo';
+
+  if(accordionList.length) { // temos que verificar se existe, se não, dará erro
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    
+    function activeAccordion() { // ao clicar, adicionar 'ativo' ao próximo elemento da lista
+      this.classList.toggle(activeClass); // ao invés do add, colocar toggle para quando clicar duas vezes abrir e fechar
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion)
+    })
+  }
+}
+initAccordion();
+ 
+
+
+
 
 
 
---
+++ scroll-suave-link-interno/script.js
@@ -0,0 +1,74 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  }
+}
+initAccordion();
+
+// SCROLL SUAVE PARA LINKS INTERNOS - esse scroll suave não funciona além de firefox e chrome, mais pra frente faremos uma função nativa
+// ao invés de você clicar no link interno e ele pular, de forma dura, pro local da tela que nos leva esse link interno, faremos um scroll suave
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]'); // links que começam com #
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href); // href é igual ao id das section 
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo(0,topo);
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+  // a relação que temos com href do link e do elemento, é o id do elemento, deve ser o mesmo do href #idDoElemento
+  // o .scrollIntoView() está relacionado ao elemento em si
+}
+initScrollSuave();
