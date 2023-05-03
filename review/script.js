47a8d5abde5fb1625bbe42377bbe1569e9f00eac
Leticia Lopes <leticialopes236@gmail.com>
2023-05-03 15:32:56 -0300

    revisando

--- review/script.js
+++ review/script.js
@@ -191,48 +191,76 @@ console.log(total);
 
 
 
-
-
 // Retorne o url da página atual utilizando o objeto window
+const url = window.location.href
+console.log(url)
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
+const activeClass = document.querySelector('.ativo')
+console.log(activeClass)
 
 // Retorne a linguagem do navegador
+const language = window.navigator.language
 
 // Retorne a largura da janela 
-
-
+const largura = window.innerWidth;;
+console.log(largura)
 
 
 
 // Retorne no console todas as imagens do site
+const imagens = document.querySelectorAll('img')
+for (let i = 0; i < imagens.length; i++) {
+  console.log(imagens[i])
+}
+
+//or
+
+imagens.forEach((imagem, index) => {
+  console.log(imagem, index)
+})
+
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const imag = document.querySelectorAll('img[src^="img/imagem"]') // aspas depois do = e antes de escrever qualquer coisa
+console.log(imag)
 
 // Selecione todos os links internos (onde o href começa com #)
+const internalLinks = document.querySelectorAll('[href^="#"]')
 
 // Selecione o primeiro h2 dentro de .animais-descricao
+const descriptionAnimals = document.querySelector('.animais-descricao')
+const resultH2 = descriptionAnimals.querySelector('h2')
+console.log(resultH2)
 
 // Selecione o último p do site
-
+const paragrafos = document.querySelectorAll('p')
+console.log(paragrafos[paragrafos.length - 1]) // podemos acessar como array pois é array-like
 
 
 
 
 // Mostre no console cada parágrado do site
+for (let i = 0; i < paragrafos.length; i++) {
+  console.log(paragrafos[i])
+}
 
 // Mostre o texto dos parágrafos no console
+paragrafos.forEach((p) => {
+  console.log(p.innerText)
+})
+
 
 // Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
 
-imgs.forEach((item, index) => {
+imgs.forEach((item, index) => { // mais de um parâmetro devemos colocar os parênteses
   console.log(item, index);
 });
 
 let i = 0;
-imgs.forEach(i => {
+imgs.forEach(() => { // mesmo sem parâmetro, precisa do parênteses
   console.log(i++);
 });
 
@@ -243,29 +271,78 @@ imgs.forEach(() => i++);
 
 
 // Adicione a classe ativo a todos os itens do menu
+const menu = document.querySelectorAll('.menu a')
+menu.forEach((item) => {
+  item.classList.add('ativo')
+});
+
 
 // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menu.forEach((item) => {
+  item.classList.remove('ativo')
+})
+menu[0].classList.add('ativo')
+
 
 // Verifique se as imagens possuem o atributo alt
+  //const imgs = document.querySelectorAll('img');
+  for (let i = 0; i < imgs.length; i++) {
+    const temAttr = imgs[i].hasAttribute('alt')
+    console.log(imgs[i], temAttr)
+  }
+
 
 // Modifique o href do link externo no menu
-
-
+const linkExterno = document.querySelector('.menu a[href^="http"]') // funciona
+linkExterno.setAttribute('href', 'JesusIsKing')
 
 
 
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
+const firstImg = document.querySelector('img')
+console.log(firstImg.offsetTop);
+
 
 // Retorne a soma da largura de todas as imagens
+function somaImagens() {
+  const todasAsImgs = document.querySelectorAll('img')
+  let soma = 0;
+  todasAsImgs.forEach((img) => {
+    soma = soma + img.offsetWidth
+    // soma += img.offsetWidth
+  })
+  console.log(soma)
+}
+window.onload = function() {
+  somaImagens()
+}
 
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
+const links = document.querySelectorAll('a')
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+
+  if(linkWidth > 48 && linkHeight > 48) {
+    console.log(link, 'Possui boa acessibilidade')
+  } else {
+    console.log(link, 'Não possui boa acessibilidade')
+  }
+})
+console.log(links)
+
+
 
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
-
+const browser = window.matchMedia('(max-width: 720px)').matches; // 'se acontecer do browser tiver no máximo 720px'
+if(browser) {
+  const menu = document.querySelector('.menu')
+  menu.classList.add = 'menu-mobile'
+}
 
 
 
@@ -285,17 +362,72 @@ img.addEventListener('click', () => {
 })
 
 
+// Quando o usuário clicar nos links internos do site, 
+// adicione a classe ativo ao item clicado e remova dos demais itens caso eles possuam a mesma. 
+// Previna o comportamento padrão desses links
+const internLinks = document.querySelectorAll('a[href^="#"]')
+
+function handleClick(event) {
+  event.preventDefault();
+  internLinks.forEach((item) => {
+    item.classList.remove('ativo')
+  })
+  event.currentTarget.classList.add('ativo')
+}
+
+internLinks.forEach((link) => {
+  link.addEventListener('click', handleClick)
+})
+
+// Selecione todos os elementos do site começando a partir do body, 
+// ao clique mostre exatamente quais elementos estão sendo clicados
+function handleElement(event) { 
+  console.log(event.currentTarget)
+  event.currentTarget.remove()
+}
+
+const all = document.querySelectorAll('body *')
+all.forEach((element) => {
+  element.addEventListener('click', handleElement)
+})
 
 
+// Utilizando o código anterior, ao invés de mostrar no console, remova o 
+// elemento que está sendo clicado, o método remove() remove um elemento
+/*   feito   */
+
+// Se o usuário clicar na tecla (t), aumente todo o texto do site. 
+function handleClick(event) {
+  console.log(event.key)
+  if(event.key === 't') {
+    document.documentElement.classList.toggle('textomaior')
+  }
+}
+
+window.addEventListener('keydown', handleClick)
+
+// const todosElementos = document.querySelectorAll('body *'); // => todos os itens dentro de body
 
 
 // Duplique o menu e adicione ele em copy
+const meniu = document.querySelector('.menu')
+const copy = document.querySelector('.copy')
+const menuDuplicado = meniu.cloneNode(true)
+copy.appendChild(menuDuplicado)
 
 // Selecione o primeiro DT da dl de Faq
+const faq = document.querySelector('.faq')
+const primeiroDt = faq.querySelector(dt) // primeiro dt
+console.log(dt)
 
 // Selecione o DD referente ao primeiro DT
+const proximoDD = primeiroDt.nextElementSibling;
+console.log(proximoDD)
 
 // Substitua o conteúdo html de .faq pelo de .animais
+const animals = document.querySelector('.animais')
+faq.innerHTML = animals.innerHTML
+// faq.innerHTML = '' // some 
 
 
 
