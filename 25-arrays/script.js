059c642ab460e37bbed6dc70626f4289e9ce08fe
Leticia Lopes <leticialopes236@gmail.com>
2023-05-12 23:36:43 -0300

    array métodos e propriedades

---
+++ 25-arrays/script.js
@@ -0,0 +1,42 @@
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
+
+const dados = [new String('Tipo 1'), ['Carro', 'Portas', {cor: 'Azul', preco: 2000}], function andar(nome) { console.log(nome) }];
+
+dados[2]('Ford');
+dados[1][2].cor; // azul
+
+
+const li = document.querySelectorAll('li');
+const arrayLi = Array.from(li)
+
+console.log(li) // retorna um array-like, parece um array mas não é
+console.log(arrayLi)
+
+
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
+
+
+const linguagens = ['html', 'css', 'js', 'php', 'python'];
+linguagens.join(); // 'html,css,js,php,python'
+linguagens.join(' '); // 'html css js php python'
+linguagens.join('-_-'); // 'html-_-css-_-js-_-php-_-python'
+
+let htmlString = '<h2>Título Principal</h2>'
+htmlString = htmlString.split('h2');
+// ['<', '>Título Principal</', '>']
+htmlString = htmlString.join('h1');
+// <h1>Título Principal</h1>
+
+
+
+
+
+
+
