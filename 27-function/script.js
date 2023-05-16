27277c8e3f1f12bd06983c7fd190624f7b5e4d5f
Leticia Lopes <leticialopes236@gmail.com>
2023-05-16 18:44:15 -0300

    functions métodos e propriedades

--- 27-function/script.js
+++ 27-function/script.js
@@ -48,20 +48,20 @@ function Dom(seletor) {
   this.element = document.querySelector(seletor);
 };
 
-Dom.prototype.ativo = function(classe) {
-  this.element.classList.add(classe);
-};
+// Dom.prototype.ativo = function(classe) {
+//   this.element.classList.add(classe);
+// };
 
-const lista = new Dom('ul');
-lista.ativo('ativar');
-console.log(lista);
+// const lista = new Dom('ul');
+// lista.ativo('ativar');
+// console.log(lista);
 
 
 const novoSeletor = {
   element: document.querySelector('li')
 }
 
-Dom.prototype.ativo.call(novoSeletor, 'ativar');
+//Dom.prototype.ativo.call(novoSeletor, 'ativar');
 
 
 Array.prototype.mostreThis = function() {
@@ -143,13 +143,34 @@ imc180(70); // 21.6
 
 // Retorne a soma total de caracteres dos
 // parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+  return acumulador + item.innerText.length
+}, 0)
+console.log(totalCaracteres)
 
 
 // Crie uma função que retorne novos elementos
 // html, com os seguintes parâmetros
 // tag, classe e conteudo.
+function criarElemento(tag, classe, conteudo) {
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : ''; // adicionamos esse ternário para que, caso eu não coloque classe, não retorne undefined, nesse caso não retorna nada com a '', posso também colocar null
+  conteudo ? elemento.innerHTML = conteudo : null;
+
+  return elemento;
+}
+
+console.log(criarElemento('li', 'azul', 'esse é o conteúdo'))
+
 
 
 // Crie uma nova função utilizando a anterior como base
 // essa nova função deverá sempre criar h1 com a
 // classe titulo. Porém o parâmetro conteudo continuará dinâmico
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo');
+
+const cursosJS = h1Titulo('Cursos de Javascript')
+const cursosHTML = h1Titulo('Cursos de HTML')
+
+console.log(cursosJS, cursosHTML)
