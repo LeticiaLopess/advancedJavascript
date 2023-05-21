99e745682e1b2ce3caf1f1e79a26c0bb5d0435f1
Leticia Lopes <leticialopes236@gmail.com>
2023-05-21 19:14:54 -0300

    modal

---
+++ 31-modal/js/modules/scroll-animacao.js
@@ -0,0 +1,26 @@
+// ANIMAÇÃO AO SCROLL
+
+export default function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]')
+
+  if(sections.length) {
+    const windowSessenta = window.innerHeight * 0.6;
+  
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top; // se não vai ficar um espação grandão branco na tela
+        const isSectionVisible = sectionTop - windowSessenta
+        if(isSectionVisible < 0) { // se passar do topo
+          section.classList.add('ativo')
+        } else {
+          section.classList.remove('ativo')
+        }
+      })
+    }
+    
+    animaScroll() // ativa pelo menos uma vez, se não os itens só vão aparecer quando eu der o primeiro scroll
+    
+    window.addEventListener('scroll', animaScroll)  
+  }
+}
+
