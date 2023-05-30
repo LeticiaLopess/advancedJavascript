fcb1444e8871954071865e961ac03223a62f620d
Leticia Lopes <leticialopes236@gmail.com>
2023-05-30 13:01:58 -0300

    setTimeout

--- 33-settimeout-setinterval/script.js
+++ 33-settimeout-setinterval/script.js
@@ -1,51 +1,92 @@
-function espera(texto) {
-  console.log(texto);
+// function espera(texto) {
+//   console.log(
+//     texto
+//   );
+// }
+// setTimeout(espera, 2, 'Passou 1s')
+
+
+// setTimeout(() => { // entra na fila se não passarmos valor
+//   console.log('Após 0s?')
+// })
+
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', handleClick);
+
+// function handleClick(event) {
+//   setTimeout(function() {
+//     this.classList.add('active');
+//   }, 1000)
+// }
+// // Erro pois window.classList não existe
+
+
+// const btnn = document.querySelector('button');
+// btnn.addEventListener('click', handleClick);
+
+// // this agora é btnn.
+// function handleClick(event) {
+//   setTimeout(() => {
+//     this.classList.add('active');
+//   }, 1000)
+// }
+
+// function loop(texto) {
+//   console.log(texto);
+// }
+// setInterval(loop, 1000, 'Passou 1s');
+
+// // loop a cada segundo
+// let i = 0;
+// setInterval(() => {
+//   console.log(i++);
+// }, 1000);
+
+// const contarAte10 = setInterval(callback, 1000);
+
+// let y = 0;
+// function callback() {
+//   console.log(y++);
+//   if (y > 10) {
+//     clearInterval(contarAte10);
+//   }
+// }
+
+
+// Mude a cor da tela para azul e depois para vermelho a cada 2s.
+function mudarClasse() {
+  document.body.classList.toggle('active');
 }
-setTimeout(espera, 2, 'Passou 1s')
 
+//setInterval(mudarClasse, 2000);
 
-setTimeout(() => { // entra na fila se não passarmos valor
-  console.log('Após 0s?')
-})
+// Crie um cronometro utilizando o setInterval. Deve ser possível
+// iniciar, pausar e resetar (duplo clique no pausar).
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', handleClick);
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const tempo = document.querySelector('.tempo')
 
-function handleClick(event) {
-  setTimeout(function() {
-    this.classList.add('active');
-  }, 1000)
-}
-// Erro pois window.classList não existe
+iniciar.addEventListener('click', iniciarTempo)
+pausar.addEventListener('click', pausarTempo)
+pausar.addEventListener('dblclick', resetarTempo) // clicando 2x no pausar, reseta
 
-
-const btnn = document.querySelector('button');
-btnn.addEventListener('click', handleClick);
-
-// this agora é btnn.
-function handleClick(event) {
-  setTimeout(() => {
-    this.classList.add('active');
-  }, 1000)
-}
-
-function loop(texto) {
-  console.log(texto);
-}
-setInterval(loop, 1000, 'Passou 1s');
-
-// loop a cada segundo
 let i = 0;
-setInterval(() => {
-  console.log(i++);
-}, 1000);
+let timer;
 
-const contarAte10 = setInterval(callback, 1000);
+function iniciarTempo() {
+  timer = setInterval(() => {
+    tempo.innerText = i++;
+  }, 100);
+  iniciar.setAttribute('disabled', '');
+}
 
-let y = 0;
-function callback() {
-  console.log(y++);
-  if (y > 10) {
-    clearInterval(contarAte10);
-  }
+function pausarTempo() {
+  clearInterval(timer);
+  iniciar.removeAttribute('disabled');
+}
+
+function resetarTempo() {
+  tempo.innerText = 0;
+  i = 0;
 }
