aa3352c4c57278aac87a8883e84b4b9d338aef85
Leticia Lopes <leticialopes236@gmail.com>
2023-06-27 20:10:42 -0300

    +regex

--- regular-expression/regexp-selecao/script.js
+++ regular-expression/regexp-selecao/script.js
@@ -12,3 +12,11 @@ console.log(fraseNova)
 /[A-z]/g // - não pega as letras com acentos, olhe a tabela unicode no google
 
 let regex = /[^.]/g // ^ dentro do [] é negação, ali eu seleciono tudo menos o .    :)
+
+const regexp = /^\w+/g // informa que a busca deve ser iniciada no início da linha, vai buscar o primeiro conjunto de alfanumericos antes de um caracter especial 
+
+const regex2 = /[\wã]+/gi  // -> selecionando cada palavra
+
+const grupoDeCaptura = /(\w+)@([\w.]+)/gi 
+
+const selecionaCadaPalavraSemEspaco = /\w+/gi // (i é o case insensitive)
