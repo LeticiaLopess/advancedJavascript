c861662586ec23619567ef7e17591f6a177855ab
Leticia Lopes <leticialopes236@gmail.com>
2023-06-27 22:08:20 -0300

    + regex

--- regular-expression/regexp-selecao/script.js
+++ regular-expression/regexp-selecao/script.js
@@ -185,7 +185,6 @@ while(regexp4.test(frase2)) {
 
 
 
-
 const frase3 = 'JavaScript, TypeScript, CoffeeScript, Java'; 
 const regexp5 = /\w+/g
 
@@ -200,11 +199,113 @@ while(regexpResult = regexp5.exec(frase3) !== null) {
 
 
 
+// str.match()
+
+const regexp = /\w{2,}/g;
+const regexpSemG = /\w{2,}/;
+const frase = 'JavaScript, TypeScript e CoffeeScript';
+
+frase.match(regexp);
+// ['JavaScript', 'TypeScript', 'CoffeeScript']
+
+frase.match(regexpSemG);
+// ["JavaScript", index: 0, input: "JavaScript,
+// TypeScript e CoffeeScript", groups: undefined]
 
 
 
 
+// str.split()
 
+const frase = 'JavaScript, TypeScript, CoffeeScript';
+
+frase.split(', ');
+// ["JavaScript", "TypeScript", "CoffeeScript"]
+frase.split(/Script/g);
+// ["Java", ", Type", ", Coffee", ""]
+
+const tags = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+tags.split(/(?<=<\/?)\w+/g).join('div');
+// <div>
+//   <div>Item 1</div>
+//   <div>Item 2</div>
+// <div>
+
+
+
+
+// str.replace()
+
+const tags = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+tags.replace(/(?<=<\/?)\w+/g, 'div'); // seleciona o nome de todas as tags pra mudar todas ao mesmo tempo
+tags.replace(/ul/g, 'div'); // pega todas as ul e muda pra div
+// <div>
+//   <div>Item 1</div>
+//   <div>Item 2</div>
+// <div>
+
+
+const tags = `
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+</ul>
+`;
+
+tags.replace(/<li/g, '$& class="ativo"');
+// <ul>
+//   <li class="ativo">Item 1</li>
+//   <li class="ativo">Item 2</li>
+// </ul>
+
+
+
+const emails = `
+empresa@email.com
+contato@email.com
+suporte@email.com
+`;
+
+emails.replace(/(\w+@)[\w.]+/g, '$1gmail.com');
+// empresa@gmail.com
+// contato@gmail.com
+// suporte@gmail.com
+
+
+
+const regexp = /(\w+)(@[\w]+)/g;
+const emails = `joao@homail.com.br
+marta@ggmail.com.br
+bruna@oulook.com.br`
+
+emails.replace(regexp, function(...args) {
+  console.log(args);
+  if(args[2] === '@homail') {
+    return `${args[1]}@hotmail`;
+  } else if(args[2] === '@ggmail') {
+    return `${args[1]}@gmail`;
+  } else if(args[2] === '@oulook') {
+    return `${args[1]}@outlook`;
+  } else {
+    return 'x';
+  }
+});
+
+// joao@hotmail.com.br
+// marta@gmail.com.br
+// bruna@outlook.com.br
 
 
 
