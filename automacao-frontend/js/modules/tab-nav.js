acc748380265baa2ae92a0588d8519cca07ac2d8
Leticia Lopes <leticialopes236@gmail.com>
2023-07-06 09:46:50 -0300

    refatorando tabNav

--- automacao-frontend/js/modules/tab-nav.js
+++ automacao-frontend/js/modules/tab-nav.js
@@ -1,22 +1,56 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+// export default function initTabNav() {
+//   const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+//   const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime
-    tabContent[index].classList.add('ativo', direcao); // adiciona a classe show-right e show-down
+//   function activeTab(index) {
+//     tabContent.forEach((section) => {
+//       section.classList.remove('ativo');
+//     });
+//     const direcao = tabContent[index].dataset.anime
+//     tabContent[index].classList.add('ativo', direcao); // adiciona a classe show-right e show-down
+//   }
+
+//   if(tabMenu.length && tabContent.length) {
+//     tabContent[0].classList.add('ativo');
+
+//     tabMenu.forEach((itemMenu, index) => {
+//       itemMenu.addEventListener('click', () => {
+//         activeTab(index);
+//       });
+//     });
+//   }
+// }
+
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu);
+    this.tabContent = document.querySelectorAll(content);
+    this.activeClass = 'ativo';
   }
 
-  if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+  // ativa a classe de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((section) => {
+      section.classList.remove(this.activeClass);
+    });
+    const direcao = this.tabContent[index].dataset.anime
+    this.tabContent[index].classList.add(this.activeClass, direcao); // adiciona a classe show-right e show-down
+  }
 
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => thid.activeTab(index));
     });
   }
+
+  init() {
+    if(this.tabMenu.length && this.tabContent.length) {
+      // ativa o primeiro item
+      this.activeTab(0)
+      this.addTabNavEvent();
+    }
+  }
+
+  
 }
+
